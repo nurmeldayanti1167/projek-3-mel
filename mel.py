433234ea50ebf22fdcb70101f70770fbacba538c
nurmeldayanti1167 <nurmeldayanti58@gmail.com>
2026-04-16 09:03:30 +0800

    commit 5

--- mel.py
+++ mel.py
@@ -6,3 +6,5 @@ for k, v in catatan.items():
     print(f"- {k}: Rp{v}")
     total = sum(catatan.values())
 print(f"Total Pengeluaran: Rp{total}")
+# Logic: catatan.pop(item_terakhir)
+print("Catatan berhasil diperbarui!")
